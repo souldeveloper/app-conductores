f51db4ca9adfb4ab488d64c5c50fe43abb84f187
manuel <souldeveloper@gmail.com>
2025-06-14 12:49:15 +0200

    niveles de zoom

--- src/components/NuevaBusqueda copy 3.jsx
+++ src/components/NuevaBusqueda copy 3.jsx
@@ -82,7 +82,7 @@ const getColor = tipo => {
 const MY_HOTELS_KEY = 'myHotels';
 
 // niveles de zoom para los botones (ajústalos aquí)
-const zoomLevels = [13, 14, 15, 16, 16];
+const zoomLevels = [14, 15, 16, 18, 20];
 
 const MapaConductor = () => {
   const navigate = useNavigate();
